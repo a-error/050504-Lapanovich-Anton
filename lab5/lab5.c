e834e0f6f24073b2257dc205352ee0ab4b3e4113
a-error <71089904+a-error@users.noreply.github.com>
2020-10-17 13:05:35 +0300

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include <malloc.h>
 
+
 int main() {
 
     setlocale(LC_ALL, "Russian");
@@ -12,50 +13,50 @@ error:
     scanf_s("%d", &n);
     system("cls");
 
-    if (n <= 0) {    // проверка допустимых значений
-        printf("Порядок матрицы должен быть больше нуля, пропробуйте ещё раз!\n");
+    if (n <= 0) {   // проверка допустимых значений
+        printf("Порядок матрицы должен быть больше нуля, задайте другое значение!\n");
 
         goto error;
     }
-    A = (int**)malloc(n * sizeof(int*));    // выделение памяти для строк
+    A = (int**)malloc(n * sizeof(int*));    // выделение памяти для массивов
     B = (int**)malloc(n * sizeof(int*));
 
-    for (i = 0; i < n; i++) {    // выделение памяти для элементов строк
+    for (i = 0; i < n; i++) {   // выделение памяти для строк
         A[i] = (int*)malloc(n * sizeof(int));
         B[i] = (int*)malloc(n * sizeof(int));
 
-        for (j = 0; j < n; j++) {    // заполнение матрицы A
+        for (j = 0; j < n; j++) {   // заполнение матрицы A
             A[i][j] = k;
             k++;
         }
     }
     printf(" Ваша матрица: \n");
-    for (i = 0; i < n; i++) {    // вывод матрицы  A
+    for (i = 0; i < n; i++) {   // вывод матрицы A
 
         for (j = 0; j < n; j++) printf("%5d ", A[i][j]);
         printf("\n");
     }
 
-    for (i = 0; i < n; i++) {     // транспонирование матрицы A
+    for (i = 0; i < n; i++) {   // транспонирование матрицы A
 
         for (j = 0; j < n; j++) B[j][i] = A[i][j];
     }
     printf("\n Транспонированная матрица: \n");
 
-    for (i = 0; i < n; i++) {    // вывод транспонированной матрицы B
+    for (i = 0; i < n; i++) {   // вывод матрицы B
 
         for (j = 0; j < n; j++) printf("%5d ", B[i][j]);
         printf("\n");
     }
     printf("\n");
 
-    for (i = 0; i < n; i++) {    // очистка памяти строк
+    for (i = 0; i < n; i++) {   // очистка памяти строк
         free(A[i]);
         free(B[i]);
     }
     free(A);    // очистка памяти массива
-    free(B);    
-    getchar();    getchar();    // убирает информацию о возвращении 0
+    free(B);
+    getchar();  getchar();    // убирает информацию о возвращении 0
 
     return 0;
 }
