f60457c602b05743dbb21dff5aea4306602580b4
a-error <71089904+a-error@users.noreply.github.com>
2020-10-21 23:03:09 +0300

    Update lab6.c

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -6,46 +6,46 @@
 int main() {
 
 	setlocale(LC_ALL, "Russian");
-	int** A, * arr, temp, i, j, row1, col1;
+	int** A, * arr, temp, i, j, row, col;
 
 	do {
 		printf("Задайте размеры матрицы: ");
-		scanf_s("%d%d", &row1, &col1);
+		scanf_s("%d%d", &row, &col);
 		system("cls");
 
-		if ((row1 <= 0) || (col1 <= 0)) {	// проверка на допустимость значений
+		if ((row <= 0) || (col <= 0)) {	// проверка на допустимость значений
 			printf("Задаваемые значения должны быть больше нуля.\n");
 
 			continue;
 		}
-	} while ((row1 <= 0) || (col1 <= 0));
-	A = (int**)malloc(row1 * sizeof(int*));	// выделение памяти под указатели на строки матрицы
-	arr = (int*)calloc(row1, sizeof(int));	// выделение памяти под хранение элементов массива
+	} while ((row <= 0) || (col <= 0));
+	A = (int**)malloc(row * sizeof(int*));	// выделение памяти под указатели на строки матрицы
+	arr = (int*)calloc(row, sizeof(int));	// выделение памяти под хранение элементов массива
 	printf("Заданная матрица:\n");
 
-	for (i = 0; i < row1; i++) {	// заполнение и вывод матрицы
-		A[i] = (int*)malloc(col1 * sizeof(int));	// выделение памяти под хранение элементов матрицы
+	for (i = 0; i < row; i++) {	// заполнение и вывод матрицы
+		A[i] = (int*)malloc(col * sizeof(int));	// выделение памяти под хранение элементов матрицы
 
-		for (j = 0; j < col1; j++) {
+		for (j = 0; j < col; j++) {
 			A[i][j] = rand() % 6 - 5;	// числа от -5 до 0
 			printf("%4d", A[i][j]);
 		}
 		printf("\n");
 	}
 
-	for (i = 0; i < row1; i++) {
+	for (i = 0; i < row; i++) {
 
-		for (j = 0; j < col1; j++) {	// заполнение массива отрицательными числами
+		for (j = 0; j < col; j++) {	// заполнение массива отрицательными числами
 
 			if (A[i][j] >= 0) {
 				continue;
 			}
-			arr[j] = A[i][j];	
+			arr[j] = A[i][j];
 		}
 
-		for (int k = 0; k < col1; k++) {	// сортировка отрицательных чисел
+		for (int k = 0; k < col; k++) {	// сортировка отрицательных чисел
 
-			for (j = 0; j < col1; j++) {
+			for (j = 0; j < col; j++) {
 
 				if (arr[j] >= 0) {
 					continue;
@@ -59,7 +59,7 @@ int main() {
 			}
 		}
 
-		for (j = 0; j < col1; j++) {	// заполнение строки после сортировки
+		for (j = 0; j < col; j++) {	// заполнение строки после сортировки
 
 			if (A[i][j] >= 0) {
 				continue;
@@ -67,21 +67,21 @@ int main() {
 			A[i][j] = arr[j];
 		}
 
-		for (j = 0; j < col1; j++) {	// обнуление массива
+		for (j = 0; j < col; j++) {	// обнуление массива
 			arr[j] = 0;
 		}
 	}
 	printf("\nПолученная матрица:\n");
 
-	for (i = 0; i < row1; i++) {	// вывод матрицы
+	for (i = 0; i < row; i++) {	// вывод матрицы
 
-		for (j = 0; j < col1; j++) {
+		for (j = 0; j < col; j++) {
 			printf("%4d", A[i][j]);
 		}
 		printf("\n");
 	}
 
-	for (i = 0; i < row1; i++) free(A[i]);	// освобождение памяти хранения элементов матрицы
+	for (i = 0; i < row; i++) free(A[i]);	// освобождение памяти хранения элементов матрицы
 	free(A);	free(arr);
 
 	return 0;
