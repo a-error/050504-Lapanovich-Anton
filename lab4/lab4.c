ba168046ce61c758b8854f2a23dc8fbbaba2b841
a-error <71089904+a-error@users.noreply.github.com>
2020-10-22 09:46:05 +0300

    Update lab4.c

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -2,9 +2,7 @@
 #include <locale.h>
 #include <malloc.h>
 
-
 int main() {
-
 	setlocale(LC_ALL, "Russian");
 	int* arr, n, i, j;
 
@@ -12,42 +10,33 @@ int main() {
 		printf("Задайте количество чисел: ");
 		scanf_s("%d", &n);
 		system("cls");
-
-		if (n <= 0) printf("Задаваемое значение должно быть больше нуля.\n");	// проверка на допустимость значения
-	} while (n <= 0);	
+		if (n <= 0) printf("Задаваемое значение должно быть больше нуля.\n");	
+	} while (n <= 0);	// проверка на допустимость значения
 	arr = (int*)malloc(n * sizeof(int));	// выделение памяти под хранение элементов
-	printf("Простые числа:\n");
+	printf("Простые числа:");
 
 	for (i = 0; i < n; i++) {	// заполнение и вывод массива
 		arr[i] = i;
-
-		if (i % 10 == 0) printf("\n");	// вывод матрицей
-		printf("%5d", arr[i]);
+		if (i % 10 == 0) printf("\n");
+		printf("%4d", arr[i]);
 	}
 	arr[1] = 0;	// замена на простое число
-	printf("\n\nМеньшие n-простые числа:\n");
+	printf("\n\nМеньшие n-простые числа:");
 
-	for (i = 0; i < n; i++) {
-
+	for (i = 0; i < n; i++) {	// отбор чисел согласно Решето Эратосфена
 		if (arr[i] == 0) continue;
-
 		for (j = 0; j < n; j++) {
-
 			if (arr[j] == 0) continue;
-
 			else if (j <= i) continue;
-
-			else if (arr[j] % arr[i] == 0) arr[j] = 0;	// отбор чисел согласно Решето Эратосфена, заполнение нулями
+			else if (arr[j] % arr[i] == 0) arr[j] = 0;	
 		}
 	}
 
 	for (i = 0; i < n; i++) {	// вывод массива
-
-		if (i % 10 == 0) printf("\n");	// вывод матрицей
-		printf("%5d", arr[i]);
+		if (i % 10 == 0) printf("\n");
+		printf("%4d", arr[i]);
 	}
 	free(arr);	// освобождение памяти хранения элементов
-	getchar();	getchar();
 
 	return 0;
 }
