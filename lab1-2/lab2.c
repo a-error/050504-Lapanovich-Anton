00e309d1097f169a30d46f7cc6f108d7548a8953
a-error <71089904+a-error@users.noreply.github.com>
2020-10-22 09:54:41 +0300

    Update lab2.c

--- lab1-2/lab2.c
+++ lab1-2/lab2.c
@@ -2,9 +2,7 @@
 #include <locale.h>
 #include <malloc.h>
 
-
 int main() {
-
     setlocale(LC_ALL, "Russian");
     int** A, ** B, ** C;
     int i, j, k, row1, row2, col1, col2;
@@ -15,16 +13,12 @@ int main() {
         printf("Задайте размеры второй матрицы: ");
         scanf_s("%d%d", &row2, &col2);
         system("cls");
-
-        if ((row1 <= 0) || (row2 <= 0) || (col1 <= 0) || (col2 <= 0)) {   // проверка допустимых значений
+        if ((row1 <= 0) || (row2 <= 0) || (col1 <= 0) || (col2 <= 0)) {   // проверка на допустимость вводимых значений
             printf("Задаваемые значения должны быть больше нуля.\n");
-
             continue;
         }
-
         if (row2 != col1) { // проверка на возможность умножения
             printf("Умножение невозможно! Задайте другие значения.\n");
-
             continue;
         }
     } while ((row2 != col1) || (row1 <= 0) || (row2 <= 0) || (col1 <= 0) || (col2 <= 0));
@@ -35,28 +29,31 @@ int main() {
     for (i = 0; i < row1; i++) {   // заполнение матрицы A
         A[i] = (int*)malloc(col1 * sizeof(int));    // выделение памяти под хранение строк
         C[i] = (int*)malloc(col2 * sizeof(int));
-
-        for (j = 0; j < col1; j++) A[i][j] = rand() % 21 - 10;  
+        for (j = 0; j < col1; j++) {
+            A[i][j] = rand() % 21 - 10;
+        }
     }
 
     for (i = 0; i < row2; i++) {   // заполнение матрицы B
         B[i] = (int*)malloc(col2 * sizeof(int));    // выделение памяти под хранение строк
-
-        for (j = 0; j < col2; j++) B[i][j] = rand() % 21 - 10;
+        for (j = 0; j < col2; j++) {
+            B[i][j] = rand() % 21 - 10;
+        }
     }
 
     for (i = 0; i < row1; i++) {    // заполнение матрицы С
-
         for (j = 0; j < col2; j++) {
-
-            for (k = 0; k < col1; k++) C[i][j] = A[i][k] * B[k][j]; // умножение матриц
+            for (k = 0; k < col1; k++) {
+                C[i][j] = A[i][k] * B[k][j]; // умножение матриц
+            }
         }
     }
     printf("Полученная матрица:\n");
 
     for (i = 0; i < row1; i++) {    // вывод матрицы С
-
-        for (j = 0; j < col2; j++) printf("%4d ", C[i][j]);
+        for (j = 0; j < col2; j++) {
+            printf("%4d ", C[i][j]);
+        }
         printf("\n");
     }
 
@@ -65,9 +62,10 @@ int main() {
         free(C[i]);
     }
 
-    for (i = 0; i < row2; i++) free(B[i]);  // освобождение памяти хранения строк
+    for (i = 0; i < row2; i++) {    // освобождение памяти хранения строк
+        free(B[i]);  
+    }
     free(A);     free(B);    free(C);   // освобождение памяти указателей на строки
-    getchar();  getchar();  // убирает информацию о возвращении программой нуля
 
     return 0;
 }
