a39da84a60c3ebb2eba729ee8f5be88f5dd3ee6a
a-error <71089904+a-error@users.noreply.github.com>
2020-11-28 18:10:38 +0300

    Update lab7.c

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -1,40 +1,42 @@
-#include <stdio.h>
-#include <stdlib.h>
+/* Variant 3
+*/
 
-int main()
-{
-	system("chcp 1251");
-	system("cls");
+#include <stdio.h>
+
+
+int main() {
+
 	char* s;
 	int* arr, i, n, temp = 0;
+
 	printf("Задайте размер строки: ");
-	scanf_s("%d", &n);
+	scanf("%d", &n);
 	n++;
-	s = (char*)malloc(n * sizeof(char));	// выделение памяти под хранение элементов строки
-	arr = (int*)calloc(n, sizeof(int));	// выделение памяти под храниние элементов массива
+
+	s = (char*)malloc(n * sizeof(char));	
+	arr = (int*)calloc(n, sizeof(int));	
+
 	printf("Задайте строку: ");
 	getchar();
 	fgets(s, n, stdin);
 
-	for (i = 0; i < n; i++)	// определение длин слов в строке
-	{
-		if (s[i] == ' ')	// проверка на пробел
-		{ 
-			if (i == n - 2)	// проверка на пробел в последнем элементе строки
-			{
+	for (i = 0; i < n; i++)	{
+
+		if (s[i] == ' ') {
+			if (i == n - 2)	{
 				continue;
 			}
 			arr[i] = temp;
 			temp = 0;
 			continue;
 		}
-		if (i == n - 1)	// проверка на последний элемент строки
-		{
+
+		if (i == n - 1)	{
 			arr[i] = temp;
 			break;
 		}
-		if (s[i] == '\0')	// проверка на произвольное окончание строки
-		{
+
+		if (s[i] == '\0') {
 			temp--;
 			arr[i] = temp;
 			break;
@@ -42,16 +44,16 @@ int main()
 		temp++;
 	}
 
-	for (i = 0; i < n; i++)	// поиск наименьшей длины слова
-	{
-		if ((arr[i] == 0) || (temp < arr[i]))
-		{
+	for (i = 0; i < n; i++)	{
+		if ((arr[i] == 0) 
+		|| (temp < arr[i])) {
 			continue;
 		}
 		else temp = arr[i];
 	}
+
 	printf("Длина самого короткого слова равна %d", temp);
-	free(s);	free(arr);	// освобождение памяти
+	free(s);	free(arr);	
 
 	return 0;
 }
