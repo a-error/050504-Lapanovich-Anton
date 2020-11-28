2a450e3714fcc2b660c1593286ec811a4a207298
a-error <71089904+a-error@users.noreply.github.com>
2020-11-28 18:02:01 +0300

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,60 +1,61 @@
+/* Variant 10
+*/
+
 #include <stdio.h>
-#include <locale.h>
-#include <malloc.h>
 
 int main() {
-    setlocale(LC_ALL, "Russian");
+   
     int** A, ** B, i, j, n, k = 0;
 
     do {
         printf("Задайте порядок матрицы: ");
-        scanf_s("%d", &n);
-        system("cls");
+        scanf("%d", &n);
+        system("clear");
         if (n <= 0) {
             printf("Порядок матрицы должен быть больше нуля.\n");
             continue;
         }
-    } while (n <= 0);   // проверка на допустимость задаваемых значений
-    A = (int**)malloc(n * sizeof(int*));    // выделение памяти под указатели строк
+    } while (n <= 0); 
+
+    A = (int**)malloc(n * sizeof(int*));    
     B = (int**)malloc(n * sizeof(int*));
 
-    for (i = 0; i < n; i++) {   // заполнение матрицы А
-        A[i] = (int*)malloc(n * sizeof(int));   // выделение памяти под хранение элементов
+    for (i = 0; i < n; i++) {  
+        A[i] = (int*)malloc(n * sizeof(int));  
         B[i] = (int*)malloc(n * sizeof(int));
         for (j = 0; j < n; j++) {
             A[i][j] = k;
             k++;
         }
     }
-    printf("Исходная матрица:\n");
 
-    for (i = 0; i < n; i++) {   // вывод матрицы A
+    printf("Исходная матрица:\n");
+    for (i = 0; i < n; i++) { 
         for (j = 0; j < n; j++) {
             printf("%4d", A[i][j]);
         }
         printf("\n");
     }
 
-    for (i = 0; i < n; i++) {   // транспонирование матрицы A
+    for (i = 0; i < n; i++) {  
         for (j = 0; j < n; j++) {
             B[j][i] = A[i][j];
         }
     }
-    printf("\nТранспонированная матрица:\n");
 
-    for (i = 0; i < n; i++) {   // вывод матрицы B
+    printf("\nТранспонированная матрица:\n");
+    for (i = 0; i < n; i++) {   
         for (j = 0; j < n; j++) {
             printf("%4d", B[i][j]);
         }
         printf("\n");
     }
-    printf("\n");
 
-    for (i = 0; i < n; i++) {   // освобождение памяти указателей строк
+    for (i = 0; i < n; i++) { 
         free(A[i]);
         free(B[i]);
     }
-    free(A);    free(B);    // освобождение памяти хранения элементов
+    free(A);    free(B);    
 
     return 0;
 }
