a25e33e664f9649db600c8149d4f8718ee3a312f
a-error <71089904+a-error@users.noreply.github.com>
2020-12-01 22:07:18 +0300

    Update lab9.c

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -1,37 +1,38 @@
+/* Variant 1
+*/
 #include <stdio.h>
 
 void setArrayValue(int* array, int size);
-float showAverageValue(int* array, int size);
+int showAverageValue(int* array, int size);
 
 #define SIZE 10
 
 int main(void) {
-
-    int* array; 
-    array = (int*)malloc(SIZE * sizeof(int));
-
+    
+    int array[SIZE];
+    
     setArrayValue(array, SIZE);
-    printf("Среднеарифметического этих чисел =  %.1f", 
+    printf("Среднеарифметическое этих чисел = %d", 
         showAverageValue(array, SIZE));
     
     return 0;
 }
 
 void setArrayValue(int* array, int size) {
-
+    
     printf("Задайте значения элементов массива: ");
     for (int i = 0; i < SIZE; i++) {
-        scanf("%d", &*(array+ i));
+        scanf("%d", &*(array + i));
     }
 }
 
-    float showAverageValue(int* array, int size) {
+int showAverageValue(int* array, int size) {
     
-    float AverageValue = 0;
+    int AverageValue = 0;
     for (int i = 0; i < SIZE; i++) {
         AverageValue += *(array + i);
     }
     AverageValue = AverageValue / SIZE;
-
+    
     return AverageValue;
 }
