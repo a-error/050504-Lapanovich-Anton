01ba85ab3d85f8900c49a81befaa7576467c8714
a-error <71089904+a-error@users.noreply.github.com>
2020-12-09 22:35:50 +0300

    Update lab9.c

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -7,33 +7,24 @@
 
 void setArrayValue(int* array, int size);
 float showAverageValue(int* array, int size);
-
 int main(void) {
-    
     int array[SIZE];
-    
     setArrayValue(array, SIZE);
     printf("Среднеарифметическое этих чисел = %.2f", 
         showAverageValue(array, SIZE));
-    
     return 0;
 }
-
 void setArrayValue(int* array, int size) {
-    
     printf("Задайте значения элементов массива: ");
     for (int i = 0; i < size; i++) {
         scanf("%d", &*(array + i));
     }
 }
-
 float showAverageValue(int* array, int size) {
-    
     float AverageValue = 0;
     for (int i = 0; i < size; i++) {
         AverageValue += *(array + i);
     }
-    AverageValue = AverageValue / size;
-    
+    AverageValue = AverageValue / size;  
     return AverageValue;
 }
