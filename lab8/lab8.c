d66ac488aa20c09e6a7e72f59a2a908c2c849d59
a-error <71089904+a-error@users.noreply.github.com>
2020-12-09 22:39:48 +0300

    Update lab8.c

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -4,31 +4,23 @@
 #include <stdio.h>
 
 int main(void) {
-
 	char** array;
 	int amount, length, temp, number = 0;
-
 	printf("Задайте количество и длину строк: ");
 	scanf("%d%d", &amount, &length);
 	length += 2;
-	
 	getchar();
 	array = (char**)malloc(amount * sizeof(char*));
 	for (int i = 0; i < amount; i++)    {
-
 		number++;
 		*(array + i) = (char*)malloc(length * sizeof(char));
-
 		printf("Задайте %d строку: ", number);
 		fgets(*(array + i), length, stdin);
 	}
-	
 	for (int i = 0; i < amount; i++) {
 		for (int j = 0; j < length; j++) {
-
 			number = 0;
 			while (*(*(array + i) + j) > *(*(array + i) + number)) {
-
 				temp = *(*(array + i) + j);
 				*(*(array + i) + j) = *(*(array + i) + number);
 				*(*(array + i) + number) = temp;
@@ -36,15 +28,11 @@ int main(void) {
 			}
 		}
 	}
-
 	printf("Результат сортировки:\n");
-	for (int i  = 0; i < amount; i++) {
-		
+	for (int i  = 0; i < amount; i++) {	
 		fputs(*(array + i), stdout);
 		free(*(array + i));
 	}
-    
 	free(array);
-	
 	return 0;
 }
