bc3f35c6838328e5568e4cb36be01aeb1211fddd
a-error <71089904+a-error@users.noreply.github.com>
2020-12-09 22:41:12 +0300

    Update lab7.c

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -7,56 +7,42 @@ int main(void) {
 
 	char* string;
 	int* array, length, temp = 0;
-
 	printf("Задайте длину строки: ");
 	scanf("%d", &length);
 	length++;
-
 	string = (char*)malloc(length * sizeof(char));	
 	array = (int*)calloc(length, sizeof(int));	
-
 	printf("Задайте строку: ");
 	getchar();
-
 	fgets(string, length, stdin);
 	for (int i = 0; i < length; i++)	{
 		if (*(string + i) == ' ') {
 			if (i == length - 2)	{
-
 				continue;
 			}
 			*(array + i) = temp;
 			temp = 0;
-
 			continue;
 		}
 		if (i == length - 1)	{
-
 			*(array + i) = temp;
-
 			break;
 		}
 		if (*(string + i) == '\0') {
-
 			temp--;
 			*(array + i) = temp;
-
 			break;
 		}
 		temp++;
 	}
-
 	for (int i = 0; i < length; i++)	{
 		if ((*(array + i) == 0) || (temp < *(array + i))) {
-
 			continue;
 		}
 		else temp = *(array + i);
 	}
-
 	printf("Длина самого короткого слова равна %d", temp);
 	free(string);	
         free(array);	
-
 	return 0;
 }
