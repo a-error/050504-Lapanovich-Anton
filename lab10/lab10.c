4ca28ea5e806de2280bd0320e8e9cd0b42e5f4ee
a-error <71089904+a-error@users.noreply.github.com>
2020-12-16 14:42:41 +0300

    Update lab10.c

--- lab10/lab10.c
+++ lab10/lab10.c
@@ -1,1 +1,21 @@
-10
+/* Variant 3
+*/
+#include <stdio.h>
+int divider(int a, int b, int counter);
+int main(void) {
+    int a, b, counter;
+    counter = a;
+    printf("Задайте два числа: ");
+    scanf("%d%d", &a, &b);
+    printf("Наибольший общий делитель = %d", divider(a, b, counter));
+    return 0;
+}
+int divider(int a, int b, int counter) {
+    if ((a % counter == 0) && (b % counter == 0)) {
+    return counter;
+    }
+    else {
+    counter--;
+    divider(a, b, counter);
+    }
+}
